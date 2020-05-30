8e929c473bca7832e41ca2c8b07a6e3cd714c568
Valian <mgutiers3012@gmail.com>
2020-05-30 00:55:33 -0400

    webhook

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+const express = require('express')
+const app = express()
+
+const port = 3000
+app.set('port', port)
+
+
+app.get('', (req, res)=>{
+  res.send('index')
+})
+
+
+app.listen(port, ()=> {
+  console.log('app run on port 3000')
+})
+
+
 const suma = (a, b) => {
   return a + b;
 };
