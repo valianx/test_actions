9839d4d1822be78b26ab2a6c171f47dafccbf420
Valian <mgutiers3012@gmail.com>
2020-05-30 01:20:15 -0400

    webhook

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express')
 const app = express()
+const exec = require('child_process').exec;
+
 
 const port = 3000
 console.log(port)
 app.set('port', port)
 
 
+const repo = 'https://github.com/valianx/test_actions'
+
 app.get('', (req, res)=>{
   res.send('index')
 })
 
+app.post('/github-webhook', ()=> {
+  try {
+    exec(`git pull`);
+  } catch (error) {
+    console.log(error);
+  }
+})
 
 app.listen(port, ()=> {
   console.log('app run on port 3000')
