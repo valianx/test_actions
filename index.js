268879b56d4c544321433d874edd30fe51836dfe
Valian <mgutiers3012@gmail.com>
2020-05-30 01:33:39 -0400

    auto deploy

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
-const express = require('express')
-const app = express()
-const exec = require('child_process').exec;
+const express = require("express");
+const app = express();
+const { exec } = require("child_process");
 
+const port = 3000;
 
-const port = 3000
-console.log(port)
-app.set('port', port)
+app.set("port", port);
 
+const repo = "https://github.com/valianx/test_actions";
 
-const repo = 'https://github.com/valianx/test_actions'
+app.get("", (req, res) => {
+  res.send("index");
+});
 
-app.get('', (req, res)=>{
-  res.send('index')
-})
-
-app.post('/github-webhook', ()=> {
+app.post("/github-webhook", async (req, res) => {
   try {
-    exec(`git pull origin master`);
+    exec(`cd ${__dirname} && git pull origin master`, (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    });
+    console.log("2");
+    return res.status(200).json("ok");
   } catch (error) {
     console.log(error);
+    return res.status(500);
   }
-})
+});
 
-app.listen(port, ()=> {
-  console.log('app run on port 3000')
-})
-
+app.listen(port, () => {
+  console.log("app run on port 3000");
+});
 
 const suma = (a, b) => {
   return a + b;
